72e4c11d5e668d763c4868242ac8191eb97a28c4
句和一 <alice@aisys.dev>
2023-05-27 09:32:15 -0600

    dumpster fire 🔥

--- index.js
+++ index.js
@@ -1,176 +1,167 @@
-let token = "";
-let port = 2334;
+const token = '';
+const port = 2334;
 
-const express = require("express")
+const express = require('express');
 const app = express();
-var http = require("http")
-const server = http.createServer(app)
-var socket = require("socket.io")
-const io = socket(server)
-console.log(socket.Socket)
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+console.log(socket.Socket);
 
-const bodyParser = require("body-parser")
-const moment = require("moment");
-require("moment-duration-format");
-const prettyBytes = require("pretty-bytes");
+const bodyParser = require('body-parser');
+const moment = require('moment');
+require('moment-duration-format');
+const prettyBytes = require('pretty-bytes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
-}))
-app.use(express.static(__dirname))
+}));
+app.use(express.static(__dirname));
 server.listen(port, () => {
-    console.log("Server Is Running")
-})
+    console.log('Server Is Running');
+});
 let watching = 0;
 let startStat;
-var AntiSpam = new Set();
-io.on("connection", (socket) => {
-    console.log(socket.id + "• User Connected " + new Date())
+const AntiSpam = new Set();
+io.on('connection', (socket) => {
+    console.log(socket.id + '• User Connected ' + new Date());
     watching += 1;
 
-    io.emit("test", {
-        data: `<div class="center"><h1 class="updatedAt">Updates Every: <span class="password">30 s</span><br/>Connected To: <span class="password">${client.user ? client.user.tag : " "}</span><br/><span class="password">${watching}</span> site watchers</h1><h4 class="updatedAt viewvv"> Last Updated At: <span class="password">${moment(Date.now())}</span></h4></div>`,
-        message: startStat ? startStat : "Fetching stats please refresh or return shortly"
-    })
-    socket.on("disconnect", () => {
-        console.log("User Disconnected " + socket.id)
+    io.emit('test', {
+        data: `<div class="center"><h1 class="updatedAt">Updates Every: <span class="password">30 s</span><br/>Connected To: <span class="password">${client.user ? client.user.tag : ' '}</span><br/><span class="password">${watching}</span> site watchers</h1><h4 class="updatedAt viewvv"> Last Updated At: <span class="password">${moment(Date.now())}</span></h4></div>`,
+        message: startStat ? startStat : 'Fetching stats please refresh or return shortly'
+    });
+    socket.on('disconnect', () => {
+        console.log('User Disconnected ' + socket.id);
         watching -= 1;
-    })
-})
+    });
+});
 
-const {
-    Client,
-    Intents
-} = require("discord.js")
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS]
-})
-const config = require("./config.js")
-const {
-    Manager
-} = require("erela.js")
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const config = require('./config.js');
+const { Manager } = require('erela.js');
+
 client.manager = new Manager({
     nodes: config.nodes,
     send: (id, payload) => {
         const guild = client.guilds.cache.get(id);
         if (guild) guild.shard.send(payload);
     }
-})
-client.login(token)
+});
+client.login(token);
 
-client.on("ready", (client) => {
-    console.log(client.user.tag + " ready")
-    client.manager.init(client.user.id)
-    setTimeout(() => {
-        startStat = getStat(client)?.join("\n");
-
-    }, 2000)
+client.on('ready', (client) => {
+    console.log(client.user.tag + ' ready');
+    client.manager.init(client.user.id);
     setInterval(() => {
-        getStat(client);
-        let bun = getStat(client);
-        startStat = bun?.join("\n");
-        io.emit("test", {
-            data: `<div class="center"><h1 class="updatedAt">Updates Every: <span class="password">30 s</span><br/>Connected To: <b class="password">${client.user ? client.user.tag : " "}</b><br/><span class="password">${watching}</span> site watchers</h1><br><h4 class="updatedAt viewvv"> Last Updated At: <span class="password">${moment(Date.now())}</span></h4></div>`,
+        let data = getStat(client);
+        let bun = data.all;
+        startStat = bun?.join('\n');
+        io.emit('test', {
+            data: `<div class="center"><h1 class="updatedAt">Updates Every: <span class="password">30 s</span><br/>Connected To: <b class="password">${client.user ? client.user.tag : ' '}</b><br/><span class="password">${watching}</span> site watchers</h1><br><h4 class="updatedAt viewvv"> Last Updated At: <span class="password">${moment(Date.now())}</span></h4></div>`,
             ///  message: `${names[Math.floor(Math.random()*names.length)]} : [${says[Math.floor(Math.random()*says.length)]}]`
-            message: `
-${bun.join(" \n")}
-   `
+            message: `${startStat}`
         });
-    }, 5000)
 
-})
-client.on("debug", msg => {
-    if (msg.includes("Hit a 429")) return process.kill(1)
-})
-client.manager.on("nodeConnect", node => {
-    console.log(`Node "${node.options.identifier}" connected.`)
-})
-client.manager.on("nodeError", (node, error) => {
-    console.log(`Node "${node.options.identifier}" encountered an error: ${error.message}.`)
-})
-client.on("raw", d => client.manager.updateVoiceState(d));
+        io.emit('updateValues', {
+            cpu: data.cpu,
+            mem: data.mem,
+            cardIndex: data.cardIndex
+        });
 
-//const cardsContainer = document.getElementsByClassName('cards');
+    }, 2000);
+});
+client.on('debug', msg => {
+    if (msg.includes('Hit a 429')) return process.kill(1);
+});
+client.manager.on('nodeConnect', node => {
+    console.log(`Node "${node.options.identifier}" connected.`);
+});
+client.manager.on('nodeError', (node, error) => {
+    console.log(`Node "${node.options.identifier}" encountered an error: ${error.message}.`);
+});
+client.on('raw', d => client.manager.updateVoiceState(d));
 
 function getStat(client) {
-    let all = [];
+    const all = [];
     let cardIndex = 0;
-    client.manager.nodes.forEach(node => {
-        let cpu = (node.stats.cpu.lavalinkLoad * 100).toFixed(2);
-        let mem = ((node.stats.memory.used / node.stats.memory.reservable) * 100).toFixed(2);
-        cardIndex++;
+    let cpu;
+    let mem;
+
+    client.manager.nodes.forEach((node, index) => {
+        cpu = (node.stats.cpu.lavalinkLoad * 100).toFixed(2);
+        mem = ((node.stats.memory.used / node.stats.memory.reservable) * 100).toFixed(2);
+        console.log(`potatoes again? -> ${index}`);
+        cardIndex = index;
 
         all.push(`
-      <div class="card">
-        <div style="display: inline-block; margin: 20px auto;">
-          <div class="center">
-            <h1 class="title">${node.options.identifier}</h1>
-          </div>
-          <div class="center">
-            <h1 id="connection-status" class="${node.connected ? 'connected' : 'disconnected'} ${node.connected ? 'glow-g' : 'glow-r'}">${node.connected ? 'CONNECTED' : 'DISCONNECTED'}</h1>
-          </div>
-          <div class="divider"></div>
-          <div class="center">
-            <h1>CPU USAGE</h1>
-            <h2 id="dial-value-${cardIndex}" class="dial-value">%</h2>
-          </div>
-          <div class="dial">
-            <div id="hand-pointer-${cardIndex}" class="hand"></div>
-          </div>
-          <div class="dial-inverted">
-            <div id="hand-pointer-inv-${cardIndex}" class="hand-inverted"></div>
-          </div>
-          <div class="center">
-            <h2 id="dial-value-inverted-${cardIndex}" class="dial-value-inverted">%</h2>
-            <h1>MEMORY USAGE</h1>
-          </div>
-          <div class="divider"></div>
-          <div class="center">
-            <div class="bg-ct">
-              <h3>
-                Uptime
-                <br>
-                <span class="detail">${moment.duration(node.stats.uptime).format(" d[d], h[h], m[m]")}</span>
-              </h3>
-              <h3>
-                Players
-                <br>
-                <span class="detail">${node.stats.playingPlayers}/${node.stats.players}</span>
-              </h3>
+          <div class="card">
+            <div style="display: inline-block; margin: 20px auto;">
+              <div class="center">
+                <h1 class="title">${node.options.identifier}</h1>
+              </div>
+              <div class="center">
+                <h1 id="connection-status" class="${node.connected ? 'connected' : 'disconnected'} ${node.connected ? 'glow-g' : 'glow-r'}">${node.connected ? 'CONNECTED' : 'DISCONNECTED'}</h1>
+              </div>
               <div class="divider"></div>
-              <h3>
-                Host
-                <br>
-                <span class="detail">${node.options.host}</span>
-              </h3>
-              <h3>
-                Port
-                <br>
-                <span class="detail">${node.options.port}</span>
-              </h3>
-              <h3>
-                Password
-                <br>
-                <span class="password">${node.options.password}</span>
-              </h3>
-              <h3>
-                Secure
-                <br>
-                <span class="password">${node.options.secure ? "TRUE" : "FALSE"}</span>
-              </h3>
+              <div class="center">
+                <h1>CPU USAGE</h1>
+                <h2 id="dial-value-${index}" class="dial-value"></h2>
+              </div>
+              <div class="dial">
+                <div id="hand-pointer-${index}" class="hand"></div>
+              </div>
+              <div class="dial-inverted">
+                <div id="hand-pointer-inv-${index}" class="hand-inverted"></div>
+              </div>
+              <div class="center">
+                <h2 id="dial-value-inverted-${index}" class="dial-value-inverted"></h2>
+                <h1>MEMORY USAGE</h1>
+              </div>
+              <div class="divider"></div>
+              <div class="center">
+                <div class="bg-ct">
+                  <h3>
+                    Uptime
+                    <br>
+                    <span class="detail">${moment.duration(node.stats.uptime).format(' d[d], h[h], m[m]')}</span>
+                  </h3>
+                  <h3>
+                    Players
+                    <br>
+                    <span class="detail">${node.stats.playingPlayers}/${node.stats.players}</span>
+                  </h3>
+                  <div class="divider"></div>
+                  <h3>
+                    Host
+                    <br>
+                    <span class="detail">${node.options.host}</span>
+                  </h3>
+                  <h3>
+                    Port
+                    <br>
+                    <span class="detail">${node.options.port}</span>
+                  </h3>
+                  <h3>
+                    Password
+                    <br>
+                    <span class="password">${node.options.password}</span>
+                  </h3>
+                  <h3>
+                    Secure
+                    <br>
+                    <span class="password">${node.options.secure ? 'TRUE' : 'FALSE'}</span>
+                  </h3>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <br>
-    `);
-
-        io.emit("updateValues", {
-            cpu: cpu,
-            mem: mem,
-            cardIndex: cardIndex
-        });
+          <br>
+        `);
     });
 
-    return all;
-}
+    return { all, cpu, mem, cardIndex };
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-var socket = io();
-var body = document.getElementById("Info")
+const socket = io();
+const body = document.getElementById('Info');
 
-socket.on("test",
+socket.on('test',
     function (data) {
         body.innerHTML = `${data.data}<br>
         ${data.message} `;
-    })
+    });
 
-socket.on("updateValues",
+socket.on('updateValues',
     function (data) {
         console.log(`\n\nDATA:\ncpu ${data.cpu}\ncard: ${data.cardIndex}\nmem: ${data.mem}\ncard: ${data.cardIndex}`);
         setDialHandPosition(data.cpu, data.cardIndex);
@@ -15,21 +15,45 @@ socket.on("updateValues",
     });
 
 function setDialHandPosition(percentage, cardIndex) {
-    let dialVal = document.getElementById(`dial-value-${cardIndex}`);
-    let hand = document.getElementById(`hand-pointer-${cardIndex}`);
+    let elements = Array.from(document.querySelectorAll(`#dial-value-${cardIndex}, #hand-pointer-${cardIndex}`));
+    const degrees = (percentage / 100) * 180;
 
-    const degrees = percentage * 180;
+    elements.forEach(element => {
+        if (element.id === `dial-value-${cardIndex}`) {
+            element.innerHTML = percentage + '%';
+        }
+        else if (element.id === `hand-pointer-${cardIndex}`) {
+            element.style.transform = `translate(0, -50%) rotate(${degrees - 90}deg)`;
+        }
+    });
 
-    dialVal.innerHTML = percentage + "%";
-    hand.style.transform = `translate(0, -50%) rotate(${degrees - 90}deg)`;
+    //const dialVal = document.getElementById(`dial-value-${cardIndex}`);
+    //const hand = document.getElementById(`hand-pointer-${cardIndex}`);
+
+    //const degrees = (percentage / 100) * 180;
+
+    //dialVal.innerHTML = percentage + '%';
+    //hand.style.transform = `translate(0, -50%) rotate(${degrees - 90}deg)`;
 }
 
 function setDialHandInvertedPosition(percentage, cardIndex) {
-    let dialValInv = document.getElementById(`dial-value-inverted-${cardIndex}`);
-    let handInv = document.getElementById(`hand-pointer-inv-${cardIndex}`);
-
+    let elements = Array.from(document.querySelectorAll(`#dial-value-inverted-${cardIndex}, #hand-pointer-inv-${cardIndex}`));
     const degrees = (percentage / 100) * 180;
 
-    dialValInv.innerHTML = percentage + "%";
-    handInv.style.transform = `translate(0, -50%) rotate(${180 - degrees + 90}deg)`;
+    elements.forEach(element => {
+        if (element.id === `dial-value-inverted-${cardIndex}`) {
+            element.innerHTML = percentage + '%';
+        }
+        else if (element.id === `hand-pointer-inv-${cardIndex}`) {
+            element.style.transform = `translate(0, -50%) rotate(${180 - degrees + 90}deg)`;
+        }
+    });
+
+    //const dialValInv = document.getElementById(`dial-value-inverted-${cardIndex}`);
+    //const handInv = document.getElementById(`hand-pointer-inv-${cardIndex}`);
+
+    //const degrees = (percentage / 100) * 180;
+
+    //dialValInv.innerHTML = percentage + '%';
+    //handInv.style.transform = `translate(0, -50%) rotate(${180 - degrees + 90}deg)`;
 }
